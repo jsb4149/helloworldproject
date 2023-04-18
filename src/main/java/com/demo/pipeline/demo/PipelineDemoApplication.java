50700ddeebe267dfb127fa3327d2464202d9667a
muarala <jannu.deekshitha123@gmail.com>
2023-04-18 13:56:02 +0530

    first commit

---
+++ src/main/java/com/demo/pipeline/demo/PipelineDemoApplication.java
@@ -0,0 +1,16 @@
+package com.demo.pipeline.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineDemoApplication.class, args);
+		System.out.println("hello world..!");
+		
+	}
+
+}
---
+++ src/test/java/com/demo/pipeline/demo/PipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pipeline.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
